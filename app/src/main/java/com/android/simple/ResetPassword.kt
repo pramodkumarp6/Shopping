e3aae502231ffc0f43639480e0ae297e0bdd4165
pramodkumar11 <pramodkumarp6@gmail.com>
2020-08-18 14:56:25 +0530

    Github tutorial

--- app/src/main/java/com/android/simple/ResetPassword.kt
+++ app/src/main/java/com/android/simple/ResetPassword.kt
@@ -2,11 +2,17 @@ package com.android.simple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class ResetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
         title="Forget Password"
+
+        val email = editemail.text.toString().trim()
+
+
     }
 }
