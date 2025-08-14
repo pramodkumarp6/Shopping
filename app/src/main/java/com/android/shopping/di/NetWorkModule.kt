f9638aa18e9cb8ef3f502ae36ccfb9a38c8e3c16
pramodkumarp6 <pramod.sisgain@gmail.com>
2025-08-14 11:19:05 +0530

    commit

--- app/src/main/java/com/android/shopping/di/NetWorkModule.kt
+++ app/src/main/java/com/android/shopping/di/NetWorkModule.kt
@@ -6,16 +6,23 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitClient {
+object NetWorkModule {
    // private val Auth = "Basic " + Base64.encodeToString("user:123456".toByteArray(), Base64.NO_WRAP)
    //private val AUTH = "Basic "+ Base64.encodeToString("belalkhan:123456".toByteArray(), Base64.NO_WRAP)
-    public const val  BASE_URl="http://192.168.1.100/retrofit/public/"
+   @Singleton
+   @Provides
+   fun provideOkHttpClient(): OkHttpClient {
+       return OkHttpClient.Builder()
+           // .addInterceptor(AuthInterceptor()) // optional
+           .build()
+   }
    /* private val okHttpClient =OkHttpClient.Builder()
         .addInterceptor{chain ->
             val original =chain.request()
@@ -31,7 +38,7 @@ object RetrofitClient {
 */
    @Singleton
    @Provides
-   fun provideRetrofit(): Retrofit {
+   fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
        return Retrofit.Builder()
            .baseUrl(Urls.BASE_URl)
            .addConverterFactory(MoshiConverterFactory.create())
